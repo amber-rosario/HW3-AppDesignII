8819d954525a9c41eefde05979c3d5502bedcf46
amber-rosario <114521019+amber-rosario@users.noreply.github.com>
2025-05-21 04:24:24 +0000

    Commit 1

---
+++ GadgetHub/GadgetHub.Domain/Abstract/IGadgetRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GadgetHub.Domain.Entities;
+
+namespace GadgetHub.Domain.Abstract
+{
+    public interface IGadgetRepository
+    {
+        IEnumerable<Gadget> Gadgets { get; }
+
+        void SaveGadget(Gadget gadget);
+    }
+}
---
+++ GadgetHub/GadgetHub.Domain/Entities/Gadget.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+namespace GadgetHub.Domain.Entities
+{
+    public class Gadget
+    {
+        [HiddenInput (DisplayValue = false)]
+        public int GadgetID { get; set; }
+        public string Name { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Brand { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+
+    }
+}
